12550c7b0c42922dfba886d09af53d182ea26d18
dahee0628 <ekgml3219@gmail.com>
2022-01-28 15:40:53 +0900

    add db

--- contents/models.py
+++ contents/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class PostModel(models.Model):
+    class Meta:
+        db_table = "posts"
+
+    post_id = models.CharField(max_length=10, null=False)
+    post_title = models.CharField(max_length=200, null=False)
+    post_category = models.CharField(max_length=200, null=False)
+    like_count = models.IntegerField()
+
