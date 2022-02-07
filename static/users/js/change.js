9205454fb6bb5b5f24c10a0585f0ea30915b8435
dahee0628 <ekgml3219@gmail.com>
2022-02-07 20:48:39 +0900

    서버 연결 전 업로드

---
+++ static/users/js/change.js
@@ -0,0 +1,10 @@
+let profile_img = document.querySelector('#profile');
+let change_profile_img = document.querySelectorAll(".profile_img");
+
+for(let i = 0; i < change_profile_img; i++){
+    change_profile_img[i].addEventListener("click", chage_img);
+}
+
+function chage_img(){
+    let change_profile_Attribute = this
+}
