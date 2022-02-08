8f6f5562437524a562600439a54f3723b45bf322
HANJANGWON <wkddnjsdl00@naver.com>
2022-02-08 15:17:26 +0900

    DB연결

--- db_uploader.py
+++ db_uploader.py
@@ -3,15 +3,22 @@ import csv
 import os
 import django
 
-
+# python이 실행될 때 DJANGO_SETTINGS_MODULE이라는 환경 변수에 현재 프로젝트의 settings.py파일 경로를 등록
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kizflix.settings')
+
+# python파일에서 django를 실행
 django.setup()
-CSV_PATH_POSTS = '/Users/hanjang-won/Desktop/15zizo_project_final/15team_kizflix/animation.csv'  # 경로설정 잘되었는지 확인
+
+# CSV_PATH_POSTS에 csv파일 저장
+# 경로설정 잘되었는지 확인
+CSV_PATH_POSTS = '/Users/hanjang-won/Desktop/15zizo_project_final/15team_kizflix/animation.csv'
 from contents.models import PostModel
 
+
+# csv파일을 DB에 저장하는 함수
 def insert_post():
     with open(CSV_PATH_POSTS) as in_file:
-        data_reader = csv.reader(in_file)
+        data_reader = csv.reader(in_file) # csv 읽기
         next(data_reader, None)  # 첫 행은 출력하지 않는다.
 
         for row in data_reader:
--- kizflix/settings.py
+++ kizflix/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'kizflix.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sys',
+        'USER': 'admin',
+        'PASSWORD': 'ss12345678',
+        'HOST': 'database-1.cn0nfjrjpg4e.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
